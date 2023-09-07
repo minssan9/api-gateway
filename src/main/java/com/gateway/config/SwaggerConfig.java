1b28fb72f7dcc6fbb041d8a4d5232315c8431183
sanghun.min <sanghun.min@halla.com>
2023-09-07 19:27:49 +0900

    swagger) SDP 서비스 추가

--- src/main/java/com/gateway/config/SwaggerConfig.java
+++ src/main/java/com/gateway/config/SwaggerConfig.java
@@ -1,14 +1,23 @@
 package com.gateway.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     @Bean
     public CommandLineRunner openApiGroups(
@@ -18,9 +27,22 @@ public class SwaggerConfig {
                 .getRouteDefinitions().collectList().block()
                 .stream()
                 .map(RouteDefinition::getId)
-//                .filter(id -> id.matches(".*-service"))
-//                .map(id -> id.replace("-service", ""))
-//                .map(id -> id.replace("-", "/"))
+                .filter(id -> id.matches(".*-SERVICE"))
+                .map(id -> id.replace("-SERVICE", ""))
                 .forEach(swaggerUiParameters::addGroup);
+    }
+
+
+    @Bean
+    public OpenAPI springOpenAPI(
+            @Value("${openapi.service.title}") String serviceTitle,
+            @Value("${openapi.service.version}") String serviceVersion,
+            @Value("${openapi.service.url}") String url
+    ) {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(url)))
+                .info(new Info().title(appName)
+                        .description(appName + " API 명세서")
+                        .version("v0.0.1"));
     }
 }
