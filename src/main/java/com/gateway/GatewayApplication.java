1aa865c46bb0d6ca72adaa03d1602235f63fc346
sanghun.min <sanghun.min@halla.com>
2023-09-14 13:46:18 +0900

    swagger) API 문서 통합
    - boot, cloud version leveling

--- src/main/java/com/gateway/GatewayApplication.java
+++ src/main/java/com/gateway/GatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
+
 @SpringBootApplication(scanBasePackages = {"com.gateway"})
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties({
