7a7088ac04ab0ba231011e2c3bbf0376eb391b34
sanghun.min <sanghun.min@halla.com>
2023-09-15 15:12:56 +0900

    dev) 미사용 코드 주석

--- src/test/java/com/gateway/GatewayAPITests.java
+++ src/test/java/com/gateway/GatewayAPITests.java
@@ -2,19 +2,20 @@ package com.gateway;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.junit.Assert.assertEquals;
 
 public class GatewayAPITests {
-    @Test
-    void testWhenApiCall_thenRewriteSuccess(@Autowired WebTestClient webClient) {
-        webClient.get()
-                .uri("http://localhost:" + localPort + "/v1/customer/customer1")
-                .exchange()
-                .expectBody()
-                .consumeWith((result) -> {
-                    String body = new String(result.getResponseBody());
-                    assertEquals("/api/customer1", body);
-                });
-    }
+//    @Test
+//    void testWhenApiCall_thenRewriteSuccess(@Autowired WebTestClient webClient) {
+//        webClient.get()
+//                .uri("http://localhost:" + localPort + "/v1/customer/customer1")
+//                .exchange()
+//                .expectBody()
+//                .consumeWith((result) -> {
+//                    String body = new String(result.getResponseBody());
+//                    assertEquals("/api/customer1", body);
+//                });
+//    }
 }
--- src/test/java/com/gateway/redis/RedisTest.java
+++ src/test/java/com/gateway/redis/RedisTest.java
@@ -1,64 +1,59 @@
 package com.gateway.redis;
 
-import com.gateway.account.domain.Account;
-import com.gateway.account.domain.TokenInfo;
-import com.gateway.account.repository.TokenInfoRepository;
 import org.junit.After;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("develop")
 public class RedisTest {
 
-    @Autowired
-    private final TokenInfoRepository tokenInfoRepository;
+//    @Autowired
+//    private final TokenInfoRepository tokenInfoRepository;
+//
+//    @Autowired
+//    private final RedisTemplate redisTemplate;
 
-    @Autowired
-    private final RedisTemplate redisTemplate;
-
-    public RedisTest(TokenInfoRepository tokenInfoRepository, RedisTemplate redisTemplate) {
-        this.tokenInfoRepository = tokenInfoRepository;
-        this.redisTemplate = redisTemplate;
-    }
+//    public RedisTest(TokenInfoRepository tokenInfoRepository, RedisTemplate redisTemplate) {
+//        this.tokenInfoRepository = tokenInfoRepository;
+//        this.redisTemplate = redisTemplate;
+//    }
 
     @After
     public void tearDown() throws Exception {
 //        accountRepository.deleteAll();
     }
 
-    @Test
-    public void 기본_등록_조회기능() {
-        //시작일자
-        TokenInfo tokenInfo = new TokenInfo("P14394", "test_minssan9");
-        // when
-        tokenInfoRepository.save(tokenInfo);
-        final ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(tokenInfo.getId(), tokenInfo.getToken());
-
-        // then
-        TokenInfo savedToken = tokenInfoRepository.findById(tokenInfo.getId()).get();
-
-    }
-
-    @Test
-    public void 수정기능() {
-        // given
-        String username = "nayoon";
-        Account account= (Account) Account.builder()
-            .username(username)
-            .password("1234")
-            .build();
-
-//        accountRepository.save(account);
+//    @Test
+//    public void 기본_등록_조회기능() {
+//        //시작일자
+//        TokenInfo tokenInfo = new TokenInfo("P14394", "test_minssan9");
+//        // when
+//        tokenInfoRepository.save(tokenInfo);
+//        final ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+//        valueOperations.set(tokenInfo.getId(), tokenInfo.getToken());
 //
 //        // then
-//        Account savedAccount = accountRepository.findByUsername(username).get();
-//        assertEquals(account.getUsername(), savedAccount.getUsername());
-    }
+//        TokenInfo savedToken = tokenInfoRepository.findById(tokenInfo.getId()).get();
+//
+//    }
+//
+//    @Test
+//    public void 수정기능() {
+//        // given
+//        String username = "nayoon";
+//        Account account= (Account) Account.builder()
+//            .username(username)
+//            .password("1234")
+//            .build();
+//
+////        accountRepository.save(account);
+////
+////        // then
+////        Account savedAccount = accountRepository.findByUsername(username).get();
+////        assertEquals(account.getUsername(), savedAccount.getUsername());
+//    }
 }
 
